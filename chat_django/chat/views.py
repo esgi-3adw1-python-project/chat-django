b968925b56ddde6857d82bae9e743f13e38eb6ed
patak <pataky@hotmail.fr>
2015-06-30 15:03:22 +0200

    test

--- chat_django/chat/views.py
+++ chat_django/chat/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    """Exemple page html"""
+    text = """<h1>Hello World"""
+    return HttpResponse(text)
