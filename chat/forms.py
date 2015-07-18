aaf70e8173cc24562cdbeeefc576cbcb40247223
patak <pataky@hotmail.fr>
2015-07-18 15:17:59 +0200

    Add User Management

---
+++ chat/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+
+class ConnexionForm(forms.Form):
+    username = forms.CharField(label="Nom d'utilisateur", max_length=32)
+    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
+
+
+class MessageForm(forms.Form):
+    message = forms.CharField(widget=forms.Textarea)
+
+
+
